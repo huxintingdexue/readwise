cf7d6ab7c00302b8e73eaa182471dbd68adc6c81
huxintingdexue <zhaoguang0725@163.com>
2026-03-24 16:03:20 +0800

    chore: bump sw cache version to v3

--- frontend/sw.js
+++ frontend/sw.js
@@ -1,4 +1,4 @@
-const SW_VERSION = 'v2';
+const SW_VERSION = 'v3';
 const STATIC_CACHE = `readwise-static-${SW_VERSION}`;
 const LIST_CACHE = `readwise-list-${SW_VERSION}`;
 const ARTICLE_CACHE = `readwise-article-${SW_VERSION}`;
